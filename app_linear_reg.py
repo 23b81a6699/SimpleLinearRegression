48a34f54d1e4f74b8a7c3c2e56a15898d78ac59f
23b81a6699 <23b81a6699@cvr.ac.in>
2025-12-23 11:36:21 +0530

    1st commit

---
+++ app_linear_reg.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
